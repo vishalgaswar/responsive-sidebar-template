fd3f0440ac223fde9e79850449787ddb4f3e3c8a
Vishal G. Aswar <vishalaswar25@gmail.com>
2021-04-04 11:38:15 +0530

    update

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ const sidebar = document.querySelector(".sidebar");
 toggleBtn.addEventListener("click",function(){
     sidebar.classList.toggle("show-sidebar");
 });
+
+closeBtn.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar");
+});
