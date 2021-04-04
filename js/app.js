ce9da43ad887cfbfc660e7b55361b81651819345
Vishal G. Aswar <vishalaswar25@gmail.com>
2021-04-04 11:33:05 +0530

    initial commits

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click",function(){
+    sidebar.classList.toggle("show-sidebar");
+});
